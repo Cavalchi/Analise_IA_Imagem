390e3fbba4d48f27c10de7cd988f95d7e1afcb35
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2024-11-06 10:26:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,48 +3,47 @@ from tkinter.filedialog import askopenfilename
 from imagem import ImageSelector
 import shutil
 from PIL import Image, ImageTk
-from pytorch import Net, transform, train_model  # Importar a classe Net, a função transform e a função train_model do pytorch.py
+from pytorch import Net, transform, train_model 
 import torch
 import os
 import threading
 from torchvision import datasets
-from pytorch import analyze_image_pytorch  # Importar a função analyze_image_pytorch do pytorch.py
+from pytorch import analyze_image_pytorch  
 class MainMenu:
     def __init__(self, master):
         self.master = master
         master.title("Menu principal")
-        master.geometry("1000x700")  # Ajuste para metade do tamanho da sua tela
+        master.geometry("1000x700")  
         master.configure(bg='navy')
 
-        # Get the directory of the current script'
-        # Get the directory of the current script
+    
         script_dir = os.path.dirname(os.path.abspath(__file__))
 
-        # Get the parent directory
+     
         parent_dir = os.path.dirname(script_dir)
 
-        # Join the script directory with the relative path to 'imagensreferenca'
+       
         imagensreferenca_dir = os.path.join(script_dir, 'imagensreferenca') 
 
-        # Now use 'imagensreferenca_dir' instead of 'imagensreferenca'
+       
         train_dataset = datasets.ImageFolder(imagensreferenca_dir, transform=transform)      
         trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
 
-        # Verifique se um estado de modelo salvo existe
+       
         if os.path.exists('save/model.pth'):
-            # Carregue o estado do modelo
+          
             self.model = Net()
             self.model.load_state_dict(torch.load('save/model.pth'))
         else:
-            # Treine o modelo
+          
             self.model = train_model(trainloader)
-            # Crie o diretório 'save' se ele não existir
+          
             if not os.path.exists('save'):
                 os.makedirs('save')
-            # Salve o estado do modelo
+           
             torch.save(self.model.state_dict(), 'save/model.pth')
-            # Iniciar o treinamento da rede neural em uma thread separada
-            threading.Thread(target=train_model, args=(trainloader,)).start()  # Cor de fundo azul marinho
+            
+            threading.Thread(target=train_model, args=(trainloader,)).start() 
         self.label = Label(master, text="Menu principal", bg='navy', fg='white')
         self.label.place(relx=0.5, rely=0.3, anchor='center')
         self.move_image_button = Button(master, text="Analisar e mover imagem", command=self.analyze_and_move_image)
@@ -55,53 +54,53 @@ class MainMenu:
     import shutil
 
     def analyze_and_move_image(self):
-        # Abra a janela de seleção de arquivo
+     
         filepath = askopenfilename(filetypes=[("Image files", "*.jpg *.png *.jpeg")])
 
-        if filepath:  # Se um arquivo foi selecionado
-            # Analise a imagem
+        if filepath:  
+            
             result = analyze_image_pytorch(filepath)
             print(f"A imagem foi classificada como: {result}")
             correct = input("A classificação está correta? (s/n) ")
             if correct.lower() == 's':
-                # Verifique se a pasta existe e, se não, crie a pasta
+               
                 destination_folder = f'imagensreferenca/{result}'
                 if not os.path.exists(destination_folder):
                     os.makedirs(destination_folder)
-                # Mova a imagem para a pasta correspondente
+                
                 shutil.move(filepath, destination_folder)
             elif correct.lower() == 'n':
                 correct_class = input("Qual é a classe correta? ")
-                # Verifique se a pasta existe e, se não, crie a pasta
+               
                 destination_folder = f'imagensreferenca/{correct_class}'
                 if not os.path.exists(destination_folder):
                     os.makedirs(destination_folder)
-                # Mova a imagem para a pasta correspondente
+               
                 shutil.move(filepath, destination_folder)
     def show_explanation(self):
         explanation_window = Toplevel(self.master)
         explanation_window.title("Explicação")
 
-        # Crie um frame para conter a visualização da árvore e as imagens
+       
         frame = Frame(explanation_window)
         frame.pack()
 
-        # Crie a visualização da árvore
+        
         tree = ttk.Treeview(frame)
         tree.pack(side="left")
 
-        # Crie um canvas para exibir as imagens
+       
         canvas = Canvas(frame)
         canvas.pack(side="right")
 
-        # Adicione uma barra de rolagem ao canvas
+       
         scrollbar = Scrollbar(frame, command=canvas.yview)
         scrollbar.pack(side="right", fill="y")
 
-        # Configure o canvas para usar a barra de rolagem
+        
         canvas.configure(yscrollcommand=scrollbar.set)
 
-        # Crie um frame para conter as imagens
+       
         image_frame = Frame(canvas)
         canvas.create_window((0,0), window=image_frame, anchor="nw")
 
@@ -109,7 +108,7 @@ class MainMenu:
             for p in os.scandir(path):
                 id = tree.insert(parent_node, "end", text=p.name, values=[p.path])
                 if not p.is_file():
-                    # it might have subdirectory
+                  
                     tree.insert(id, "end")
 
         def update_tree(event):
@@ -119,7 +118,7 @@ class MainMenu:
             populate_tree(selected_item, path)
 
         def show_images(event):
-            # Clear the image frame
+           
             for widget in image_frame.winfo_children():
                 widget.destroy()
 
@@ -135,34 +134,34 @@ class MainMenu:
                     image = Image.open(image_path)
                     photo = ImageTk.PhotoImage(image)
 
-                    # Add the image to the frame
+                   
                     label = Label(image_frame, image=photo)
-                    label.image = photo  # Keep a reference to the image
+                    label.image = photo  
                     label.pack()
 
-            # Update the scrollregion of the canvas after adding all the images
+            
             explanation_window.update()
             canvas.configure(scrollregion=canvas.bbox("all"))
 
-        abspath = os.path.abspath('analisepy')  # Change this to the root directory
+        abspath = os.path.abspath('analisepy') 
         root_node = tree.insert('', 'end', text=abspath, values=[abspath], open=True)
         populate_tree(root_node, abspath)
         tree.bind('<<TreeviewOpen>>', update_tree)
         tree.bind('<<TreeviewSelect>>', show_images)
     def load_image(self):
-        # Abra a janela de seleção de imagem
+        
         self.new_window = Tk()
         self.app = ImageSelector(self.new_window)
         self.new_window.mainloop()
 
     def put_image(self):
-        # Abra a janela de seleção de arquivo
+       
         filepath = askopenfilename(filetypes=[("Image files", "*.jpg *.png *.jpeg")])
 
-        if filepath:  # Se um arquivo foi selecionado
-            # Copie o arquivo para a pasta de imagens
+        if filepath:  
+            
             shutil.copy(filepath, 'imagens')
-            self.image_path = filepath  # Armazene o caminho da imagem
+            self.image_path = filepath  
 root = Tk()
 my_menu = MainMenu(root)
 root.mainloop()
